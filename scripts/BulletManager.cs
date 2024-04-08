c42ff7070c6f6a2bba55bd044678e0c13180429f
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2024-04-08 23:15:16 +0200

    Cut: useless lines

--- scripts/BulletManager.cs
+++ scripts/BulletManager.cs
@@ -5,16 +5,8 @@ public partial class BulletManager : Node2D
 {
 	private PackedScene BulletScene = GD.Load<PackedScene>("res://scenes/bullet.tscn");
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	
 	private void _on_player_shoot(Vector2 StartPos, Vector2 Direction)
 	{
-		//Area2D bulletInstance = (Area2D)BulletScene.Instantiate();
-		//Bullet bullet = bulletInstance.GetNode<Bullet>(".");
-
 		Bullet bullet = (Bullet)BulletScene.Instantiate();
 
 		bullet.Position = StartPos;
